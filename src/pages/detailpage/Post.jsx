225a5b0312f9ca8d94d2bfd1c353fa9e87574b56
cho98 <as232369@naver.com>
2022-12-27 23:38:49 +0900

    :sparkles: :feat detail, post css수정

--- src/pages/detailpage/Post.jsx
+++ src/pages/detailpage/Post.jsx
@@ -41,22 +41,24 @@ const Post = () => {
 
   return (
     <>
-      <StPost>
-        <StTitle>
-          <StInputDiv>
-            <StTitleInput
-              type="text"
-              name="content"
-              value={title}
-              onChange={onChangeTitle}
-            ></StTitleInput>
-            <StSubTitleInput
-              type="text"
-              name="content"
-              value={subtitle}
-              onChange={onChangeSubtitle}
-            />
-          </StInputDiv>
+      <StTitle>
+        {fileimage && <Stimage src={fileimage}></Stimage>}
+        <StInputDiv>
+          <StTitleInput
+            type="text"
+            name="content"
+            value={title}
+            placeholder="제목을 입력하세요"
+            onChange={onChangeTitle}
+          ></StTitleInput>
+          <StSubTitleInput
+            type="text"
+            name="content"
+            value={subtitle}
+            placeholder="소제목을 입력하세요"
+            onChange={onChangeSubtitle}
+          />
+
           <StPostLogo>
             <div>
               <StDelImg>
@@ -70,7 +72,9 @@ const Post = () => {
               onChange={saveFileimage}
             />
           </StPostLogo>
-        </StTitle>
+        </StInputDiv>
+      </StTitle>
+      <StPost>
         <StContent>
           <StContentInput
             type="text"
@@ -80,7 +84,6 @@ const Post = () => {
           />
         </StContent>
       </StPost>
-      {fileimage && <Stimage src={fileimage}></Stimage>}
     </>
   );
 };
@@ -89,43 +92,53 @@ export default Post;
 
 const StPost = styled.div`
   width: 100%;
-  border: 1px solid blue;
+  margin-top: 100px auto;
 `;
 
 const StTitle = styled.div`
-  height: 450px;
+  height: 480px;
   width: 100%;
-  border: 1px solid black;
-  margin: 0 auto;
+  border-bottom: 1px solid #6666;
+
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  display: flex;
+  z-index: 1;
 `;
 
 const StTitleInput = styled.input`
   width: 700px;
-  border: 1px solid black;
+  outline: none;
   height: 50px;
   font-size: xx-large;
   position: absolute;
+  color: black;
   bottom: 120px;
+  background: transparent;
+  mix-blend-mode: difference;
 `;
 
 const StSubTitleInput = styled.input`
   width: 700px;
-  border: 1px solid black;
+  outline: none;
   height: 30px;
   position: absolute;
+  background: transparent;
   bottom: 90px;
+  color: white;
 `;
 
 const StContent = styled.div`
   width: 700px;
   height: 300px;
-  border: 1px solid green;
+
   margin: 0 auto;
 `;
 
 const StContentInput = styled.input`
   width: 700px;
-  border: 1px solid black;
+  outline: none;
   height: 30px;
 `;
 
@@ -144,20 +157,27 @@ const StPostLogo = styled.div`
 `;
 
 const Stimage = styled.img`
-  width: 200px;
-  height: 200px;
+  width: 100%;
+  height: 450px;
 
-  border: 1px solid red;
+  object-fit: fill;
+  image-rendering: -moz-crisp-edges;
+  image-rendering: crisp-edges;
+
+  z-index: 3;
 `;
 
 const StInputDiv = styled.div`
-  border: 1px solid red;
   width: 700px;
   height: 450px;
-  margin: 0 auto;
+  margin: 150px 0px 0px 0px;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  position: relative;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  z-index: 3;
+  left: 50vw;
+  top: 25%;
 `;
